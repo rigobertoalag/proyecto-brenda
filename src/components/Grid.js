7f6b7fb4cf9e1b7fdf900d9a5f1e81addac960fa
rigobertoalag <a20150338@utem.edu.mx>
2021-05-05 16:22:39 -0500

    Finish StoreDataCache

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,37 +3,27 @@ import { Link } from 'react-router-dom'
 
 export default function Grid({ dataItems, dataCategories, isItem }) {
 
-    const [data, setData] = useState([])
-    const [data2, setData2] = useState([])
-    useEffect(() => {
-        console.log('desde useefect', data)
-    }, [data])
-
     function addToCart(id, category) {
         const cartStorage = JSON.parse(localStorage.getItem('Cart'));
         if (!cartStorage) {
             const item = ([{ 'itemID': id, 'categoryID': category }])
-            console.log('item', item)
-            setData(item)
             localStorage.setItem('Cart', JSON.stringify(item))
         } else {
-            const prevCartItems = JSON.parse([localStorage.getItem('Cart')])
-            setData(prevCartItems)
-            const item = ([{ 'itemID': id, 'categoryID': category }])
-            const newItem = ([{data, item}])
-            setData2(newItem)
-            console.log('test', data2)
-            /*const prevCartItems = JSON.parse(localStorage.getItem('Cart'))
-            const full = null //prevCartItems.unshift({ 'itemId': id, 'categoryId': category });
-            console.log('prevCartItems', prevCartItems)
-            console.log('full', full)
-            //console.log(data)
-            console.log('valio dick')*/
+            const prevItem = JSON.parse(localStorage.getItem('Cart'))
+            console.log('previtem', prevItem)
+
+            const newItem = ([{ 'itemID': id, 'categoryID': category }])
+            console.log(newItem)
+
+            Array.prototype.push.apply(prevItem, newItem);
+            console.log('desde array proto',prevItem)
+
+            localStorage.setItem('Cart', JSON.stringify(prevItem))
         }
     }
 
     const cartStorage = JSON.parse(localStorage.getItem('Cart'));
-    console.log('cartStorage', cartStorage)
+    console.log('ya convertido', cartStorage)
 
     if (dataCategories) {
         return (
@@ -65,7 +55,6 @@ export default function Grid({ dataItems, dataCategories, isItem }) {
                             <h2>{d.name}</h2>
                             <p>{d.description} el id: {d.id}</p>
                             <button onClick={() => addToCart(d.id, d.category)}>Añadir al carrito</button>
-                            {/*<button onClick={handleClick([{ id: d.id, category: d.category }])}>Añadir al carrito</button>*/}
                         </section>
                     ))
                 }
