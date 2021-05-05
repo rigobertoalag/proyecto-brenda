8443df027d3d9a1a921c74e2a3c0cfa9d1be8237
rigobertoalag <a20150338@utem.edu.mx>
2021-05-05 17:18:31 -0500

    Changes ir cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-import { items } from '../statics/items.json'
-import Grid from '../components/Grid'
+export default function Cart() {
+    const [cartItems, setCartItems] = (useState(JSON.parse(localStorage.getItem('Cart'))))
+    console.log('cartItems desde el carrito', cartItems)
 
-export default function Cart(){
-    return(
-        <main>
-            <Grid data={items}/>
-        </main>
-    )
+    function cachingDelete(){
+        localStorage.removeItem('Cart')
+        setCartItems('')
+        alert('Se borro')
+    }
+
+    if (cartItems) {
+        return (
+            <main>
+                {
+                    cartItems.map((c, i) => (
+                        <div key={i}>
+                            <li>{c.itemID}</li>
+                            <li>{c.categoryID}</li>
+                        </div>
+                    ))
+                }
+                <button onClick={cachingDelete}>Limpiar carrito</button>
+                <button onClick={()=>alert('Se envio por whats :)')}>Compartir</button>
+            </main>
+        )
+    } else {
+        return (
+            <main>
+                <h1>Nada en el carrito :(</h1>
+                <button><Link to='/categories'><a className="info-link">Ir a las categorias</a></Link></button>
+            </main>
+        )
+    }
 }
